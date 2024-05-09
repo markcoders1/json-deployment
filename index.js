64fbfd1c3dabdfdac4805ab6e5e34e99702344de
Sheryar1997 <sheryar.markcoders@gmail.com>
2024-05-10 02:24:46 +0500

    added cors config

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const jsonServer = require("json-server"); // importing json-server library
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
+const corsOptions = {
+    origin: '*',  // Allow all domains - for development only
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+server.use(cors(corsOptions));
 server.use(middlewares);
 server.use(router);
 
