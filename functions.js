6fdeec664a0ff29fe04e73fa808740c645cc12b3
Sheryar1997 <sheryar.markcoders@gmail.com>
2024-05-23 15:54:21 +0500

    added checks for press release to get for FTLF only.

--- functions.js
+++ functions.js
@@ -4,91 +4,102 @@ const Press = require("./Models/Press");
 const Stock = require("./Models/Stock");
 
 const getFillings = async () => {
-    try {
-        let config = {
-            method: "get",
-            maxBodyLength: Infinity,
-            url: "https://api.kscope.io/v2/sec/search/0001374328?key=8bc43de0-321b-4ca0-b39c-52b60b554e89&content=sec",
-            headers: {},
-        };
-        const response = await axios(config);
-        const fillingsFromDB = await Filling.find();
-        response?.data?.data?.forEach(async (filling) => {
-            const isPresent = fillingsFromDB.find(
-                (fillingFromDB) => fillingFromDB.filling?.acc === filling.acc
-            );
-            if (!isPresent) {
-                const newFilling = new Filling({
-                    filling,
-                });
-                await newFilling.save();
-                console.log("Filling saved in DB ", filling.acc);
-            }
-            console.log("Filling already present in DB ", filling.acc);
+  try {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: "https://api.kscope.io/v2/sec/search/0001374328?key=8bc43de0-321b-4ca0-b39c-52b60b554e89&content=sec",
+      headers: {},
+    };
+    const response = await axios(config);
+    const fillingsFromDB = await Filling.find();
+    response?.data?.data?.forEach(async (filling) => {
+      const isPresent = fillingsFromDB.find(
+        (fillingFromDB) => fillingFromDB.filling?.acc === filling.acc
+      );
+      if (!isPresent) {
+        const newFilling = new Filling({
+          filling,
         });
-    } catch (error) {
-        console.log("Error in getFillings", error);
-    }
+        await newFilling.save();
+        console.log("Filling saved in DB ", filling.acc);
+      }
+      console.log("Filling already present in DB ", filling.acc);
+    });
+  } catch (error) {
+    console.log("Error in getFillings", error);
+  }
 };
 
 const getPressReleases = async () => {
-    try {
-        let config = {
-            method: "get",
-            maxBodyLength: Infinity,
-            url: "https://api.kscope.io/v2/news/press-releases/FTLF?key=8bc43de0-321b-4ca0-b39c-52b60b554e89",
-            headers: {},
-        };
-        const response = await axios(config);
-        const pressReleasesFromDB = await Press.find();
-        response?.data?.data?.forEach(async (press) => {
-            const isPresent = pressReleasesFromDB.find(
-                (pressFromDB) =>
-                    pressFromDB?.press?.meta?.id === press?.meta?.id
-            );
-            if (!isPresent) {
-                const newPress = new Press({
-                    press,
-                });
-                await newPress.save();
-                console.log("Press saved in DB ", press?.meta?.id);
-            }
-            console.log("Press already present in DB ", press?.meta?.id);
-        });
-    } catch (error) {
-        console.log("Error in getPressReleases", error);
-    }
+  try {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: "https://api.kscope.io/v2/news/press-releases/FTLF?key=8bc43de0-321b-4ca0-b39c-52b60b554e89",
+      headers: {},
+    };
+    const response = await axios(config);
+    const pressReleasesFromDB = await Press.find();
+    response?.data?.data?.forEach(async (press) => {
+      if (press?.meta?.stocks?.length == 1 && press?.meta?.stocks[0] === 'FTLF') {
+        console.log("Press is for ==", press?.meta?.stocks[0], 'length of the stocks is == ', press?.meta?.stocks?.length );
+        const isPresent = pressReleasesFromDB.find(
+          (pressFromDB) =>
+            pressFromDB?.press?.meta?.id === press?.meta?.id
+        );
+        if (!isPresent) {
+          const newPress = new Press({
+            press,
+          });
+          await newPress.save();
+          console.log("Press saved in DB ", press?.meta?.id);
+        }
+        console.log("Press already present in DB ", press?.meta?.id);
+      }
+    });
+  } catch (error) {
+    console.log("Error in getPressReleases", error);
+  }
 };
 
 const getStocksData = async () => {
-    try {
-        const config = {
-            method: "get",
-            maxBodyLength: Infinity,
-            url: "https://api.polygon.io/v2/aggs/ticker/LFVN/range/1/day/2022-05-16/2024-05-16?adjusted=true&sort=asc&apiKey=_l8pALqPbP40rV7IOhsWTt36AVgrwc3X",
-            headers: {},
-        };
-        const response = await axios(config);
-        const stocksFromDB = await Stock.find();
-        response?.data?.results?.forEach(async (stock) => {
-            const isPresent = stocksFromDB.find(
-                (stockFromDB) => stockFromDB?.stock?.t === stock?.t
-            );
-            if (!isPresent) {
-                const newStock = new Stock({
-                    stock,
-                });
-                await newStock.save();
-                console.log("Stock saved in DB ", stock?.t);
-            }
-            console.log("Stock already present in DB ", stock?.t);
+  try {
+
+    const currentDate = new Date();
+    const twoYearsAgo = new Date(new Date().setFullYear(new Date().getFullYear() - 2));
+
+    // Format the dates as YYYY-MM-DD
+    const currentDateStr = currentDate.toISOString().split('T')[0];
+    const twoYearsAgoStr = twoYearsAgo.toISOString().split('T')[0];
+    console.log(currentDateStr, twoYearsAgoStr);
+    const config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `https://api.polygon.io/v2/aggs/ticker/LFVN/range/1/day/${twoYearsAgoStr}/${currentDateStr}?adjusted=true&sort=asc&apiKey=_l8pALqPbP40rV7IOhsWTt36AVgrwc3X`,
+      headers: {},
+    };
+    const response = await axios(config);
+    const stocksFromDB = await Stock.find();
+    response?.data?.results?.forEach(async (stock) => {
+      const isPresent = stocksFromDB.find(
+        (stockFromDB) => stockFromDB?.stock?.t === stock?.t
+      );
+      if (!isPresent) {
+        const newStock = new Stock({
+          stock,
         });
-    } catch (error) {
-        console.log("Error in getStocksData", error);
-    }
+        await newStock.save();
+        console.log("Stock saved in DB ", stock?.t);
+      }
+      console.log("Stock already present in DB ", stock?.t);
+    });
+  } catch (error) {
+    console.log("Error in getStocksData", error);
+  }
 };
 module.exports = {
-    getFillings,
-    getPressReleases,
-    getStocksData,
+  getFillings,
+  getPressReleases,
+  getStocksData,
 };
