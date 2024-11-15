8afb4c2261c27839a498e9e1a8ecaf9adc206b46
huzefarana <imhuzefarana@gmail.com>
2024-11-15 23:36:26 +0500

    removed redundant controllers

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
-const EsapetStripe = require('./Models/esapet-modals/Log.js');
-const Press = require("./Models/sec-filing-modals/Press.js");
 const morgan = require("morgan")
 
 // Define allowed origins
@@ -23,11 +21,7 @@ const {
 } = require("./Controllers/sec-filings-controllers/functions.js");
 const cron = require("node-cron");
 const dataFetching = require("./Controllers/sec-filings-controllers/data-fetching-controller.js");
-const stripeInfo = require("./Controllers/esapet-controllers/stripe-info-controller.js");
-const getAnalytics = require("./Controllers/first-time-controller/get-analytics.js");
 const { connect } = require("./config/Database");
-const { cloningCampaing } = require("./Controllers/sec-filings-controllers/constants.js");
-const Log = require("./Models/esapet-modals/Log.js");
 connect();
 app.use(express.json());
 app.use(cors(corsOptions));
@@ -69,9 +63,7 @@ const sportscardController = require("./Controllers/sportscard-controller.js");
 app.use("/api", dataFetching);
 
 //PARENT API FOR ESAPET STRIPE INFO
-app.use('/esapet', stripeInfo);
 app.use("/", sportscardController);
-app.use("/", getAnalytics);
 
 // Delete all Esapet logs 
 // EsapetStripe.deleteMany({}).then(() => {
